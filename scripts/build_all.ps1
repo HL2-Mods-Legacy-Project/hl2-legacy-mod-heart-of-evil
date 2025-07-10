385d3364980f2cd94b2498c142a13454d23cb9e1
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:01:43 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "HoE_101008" `
+  -ModName "Heart of Evil: Source" `
+  -ModFolder "hoe" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/heart-of-evil/hoe-101008-Original.zip" `
+  -ModBaseFilesUrlHash "e18f026a9f2e976c23b0e5302b76e34ebcfb43f4540e7e91a6887af00b81a0fb" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
